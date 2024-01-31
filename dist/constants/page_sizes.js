d6fbf4fb42970ec3f2f524500ce6b5828a622af9
Francesco Pippo <34452508+FrappaF@users.noreply.github.com>
2024-01-31 11:25:49 +0100

    Delete dist directory

--- dist/constants/page_sizes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    "A4": { maxWidth: 595, maxHeight: 841 }
-};
-//# sourceMappingURL=page_sizes.js.map
